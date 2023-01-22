0b95ed6e7007274d6132af5ffc53fda91af6ae1b
Fortunella <sherbakovdaniil6@gmail.com>
2023-01-22 18:47:59 +0300

    Update main

--- 1.py
+++
@@ -1,7 +0,0 @@
-weekday = int(input('Введите номер дня: '))
-if 1 > weekday or weekday > 7:
-    print("Такого дня нет")
-elif 0 < weekday < 6:
-    print("Рабочий")
-else:
-    print("Выходной")
--- models/export.py
+++
@@ -1,3 +0,0 @@
-def export_contact(id, name, surname, description):
-    with open('contacts.txt', 'a', encoding='utf-8') as file:
-        file.write(f'{id}|{name}|{surname}|{description}\n')
